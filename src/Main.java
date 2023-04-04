604f3cdc2deca032145c133dce7931a97420bfb5
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-04 21:33:41 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ import com.mnrega.dao.DBUtils;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        String str = "amanakak";
-
         System.out.println("Hello world!");
     }
 }
